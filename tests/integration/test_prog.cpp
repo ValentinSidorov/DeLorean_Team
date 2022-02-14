4a957e491cd649c682709796f380bc3f5fdee9bb
Serhii <writetosergiiu@gmail.com>
2022-02-14 15:48:38 +0200

    change codestyle

--- tests/integration/test_prog.cpp
+++ tests/integration/test_prog.cpp
@@ -33,11 +33,13 @@ void print_hello(void) {
   }
 
   fout.close();
+
+  exit(0);
 }
 
 void print_arg(const std::string& str) {
   int i = 0;
-  std::cout << "Output text as argument\n";
+  std::cout << "text output in console\n";
 
   for (i = 0; i < 10; i++) {
     std::cout << str << std::endl;
@@ -62,7 +64,7 @@ void print_arg(const std::string& str) {
 
 void print_arg2(const std::string& str1, const std::string& str2) {
   int i = 0;
-  std::cout << "Output text as argument\n";
+  std::cout << "Text output in console\n";
   while (i < 10) {
     std::cout << str1 << std::endl;
     std::cout << str2 << std::endl;
@@ -89,7 +91,7 @@ void print_arg2(const std::string& str1, const std::string& str2) {
 
 void print_arg3(const std::string& str3) {
   int i = 0;
-  std::cout << "Output text as argument\n";
+  std::cout << "Text output in conosle\n";
   while (i < 10) {
     std::cout << str3 << std::endl;
     i++;
@@ -128,72 +130,32 @@ void print_arg3(const std::string& str3) {
   }
 }
 
-/*void print_choice(void){
-        cout<<"Press key 1 to output entered text 10 times to stdout, create
-   file and write the text 10 times there"<<endl; cout<<endl; cout<<"Press key 2
-   to output two entered text 10 times to console, create file and write the
-   text 10 times there"<<endl; cout<<endl; cout<<"Press key 3 to output of
-   entered texts to console, create file and write the text there"<<endl;
-
-        cout<<endl;
-        }*/
-
 int main(int argc, char* argv[]) {
-  // print_choice();
+  if (argc > 1) {
+    string str(argv[1]);
 
-  print_hello();
-
-  std::stringstream convert(
-      argv[1]);  // create varibale stringstream with name convert, initializing
-                 // it with the value of argv[1]
-
-  int a;
-  if (!(convert >> a))  // make conversion
-    a = 0;  // if conversion fails, then assign a value to a by default
-
-  if (a == 1) {
-    cout << "You've chosen to output entered text 10 times to stdout, create "
-            "file and write the text 10 times there"
-         << endl;
-    cout << "Enter some text: " << endl;
-    // string str="This is text 1.";
-
-    string str;
-
-    getline(cin, str);
     print_arg(str);
 
-  }
+    if (argc < 3) {
+      exit(0);
+    }
 
-  else if (a == 2) {
-    cout << "Now output with the function of two arguments passed: " << endl;
-    cout << "Enter some text: " << endl;
-
-    string str1;
-    getline(cin, str1);
-
-    cout << "Enter another text: " << endl;
-    string str2;
-
-    getline(cin, str2);
-
+    string str1(argv[2]);
+    string str2(argv[3]);
     print_arg2(str1, str2);
 
-  }
+    if (argc < 4) {
+      exit(0);
+    }
 
-  else if (a == 3) {
-    cout << "Now output with infinite loop function taking one argument: "
-         << endl;
-    string str3;
-
-    cout << "Enter some text: " << endl;
-    getline(cin, str3);
+    string str3(argv[4]);
     print_arg3(str3);
 
+    exit(0);
   }
 
   else {
-    cout << "You have not pressed the required keys, try again" << endl;
+    print_hello();
   }
 
   return 0;

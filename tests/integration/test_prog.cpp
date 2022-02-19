ed53c49dfe8f66ab84ef80bb65fc5145b2497bee
Serhii <writetosergiiu@gmail.com>
2022-02-19 17:36:49 +0200

    updated branch

--- tests/integration/test_prog.cpp
+++ tests/integration/test_prog.cpp
@@ -1,8 +1,10 @@
 #include "include/test_prog.hpp"
 
+#include <string.h>
 #include <unistd.h>
 
 #include <cstdio>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -27,80 +29,52 @@ void print_hello(void) {
 }
 
 void print_arg(const std::string& str) {
-  int i = 0;
-  std::cout << "text output in console\n";
-
-  for (i = 0; i < 10; i++) {
-    std::cout << str << std::endl;
-  }
+  std::cout << str << std::endl;
 
   std::ofstream fout("filename1.txt");
 
   if (!fout.is_open()) {
     std::cout << "mistake in opening the file\n";
-
-  } else {
-    std::cout << "filename1.txt has been successfully created, you can check "
-                 "it out in your current directory\n";
   }
 
-  for (int i = 0; i < 10; i++) {
-    fout << str << std::endl;
-  }
+  fout << str << std::endl;
 
   fout.close();
+
+  exit(0);
 }
 
 void print_arg2(const std::string& str1, const std::string& str2) {
-  int i = 0;
-  std::cout << "Text output in console\n";
-  while (i < 10) {
-    std::cout << str1 << std::endl;
-    std::cout << str2 << std::endl;
-    i++;
-  }
+  std::cout << str1 << std::endl;
+  std::cout << str2 << std::endl;
 
   std::ofstream fout("filename2.txt");
 
   if (!fout.is_open()) {
     std::cout << "mistake in opening the file\n";
-
-  } else {
-    std::cout << "filename2.txt has been successfully created, you can check "
-                 "it out in your current directory\n";
   }
 
-  for (int i = 0; i < 10; i++) {
-    fout << str1 << std::endl;
-    fout << str2 << std::endl;
-  }
+  fout << str1 << std::endl;
+  fout << str2 << std::endl;
 
   fout.close();
+
+  exit(0);
 }
 
 void print_arg3(const std::string& str3) {
-  int i = 0;
-  std::cout << "Text output in conosle\n";
-  while (i < 10) {
-    std::cout << str3 << std::endl;
-    i++;
-  }
+  std::cout << str3 << std::endl;
 
   std::ofstream fout("filename3.txt");
 
   if (!fout.is_open()) {
     std::cout << "mistake in opening the file\n";
-
-  } else {
-    std::cout << "filename4.txt has been successfully created, you can check "
-                 "it out in your current directory\n";
+    exit(0);
   }
 
   int num = 0;
 
-  std::cout << "the program with endless loop will be finished after five "
-               "iterations, please wait"
-            << std::endl;
+  std::cout << "endless loop begins" << std::endl;
 
   while (true) {
     std::cout << str3 << std::endl;
@@ -114,31 +88,72 @@ void print_arg3(const std::string& str3) {
 }
 
 int main(int argc, char* argv[]) {
-  if (argc > 1) {
+  if (argc == 1) {
+    print_hello();
+
+    exit(0);
+
+  }
+
+  else if ((strstr(argv[1], "--text=") != NULL) && argc == 2) {
+    
+
     string str(argv[1]);
+
+    int n = str.find('=');
+
+    if (n < str.length()) {
+      str.erase(0, n + 1);
+    }
 
     print_arg(str);
 
-    if (argc < 3) {
-      exit(0);
+  }
+
+  else if (argc == 3 && (strstr(argv[1], "--text1=") != NULL) &&
+           (strstr(argv[2], "--text2=") != NULL)) {
+    string str1(argv[1]);
+
+    int n1 = str1.find('=');
+
+    if (n1 < str1.length()) {
+      str1.erase(0, n1 + 1);
     }
 
-    string str1(argv[2]);
-    string str2(argv[3]);
+    string str2(argv[2]);
+
+    int n2 = str2.find('=');
+
+    if (n2 < str2.length()) {
+      str2.erase(0, n2 + 1);
+    }
+
     print_arg2(str1, str2);
 
-    if (argc < 4) {
-      exit(0);
+    exit(0);
+
+  }
+
+  else if (argc == 3 && (strstr(argv[1], "--text=") != NULL) &&
+           argv[2] == string("--loop")) {
+
+    string str3(argv[1]);
+
+    int n3 = str3.find('=');
+
+    if (n3 < str3.length()) {
+      str3.erase(0, n3 + 1);
     }
 
-    string str3(argv[4]);
+
+
     print_arg3(str3);
 
     exit(0);
-  }
+  } else {
 
-  else {
-    print_hello();
+    cout << "You've made an error while entering the arguments, try again"
+         << endl;
   }
 
   return 0;
